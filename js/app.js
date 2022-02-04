8156db1e17eea2d625dcbe1d183f503491a389fb
Amrendra Nath <cool.amrendrasrivastav@gmail.com>
2022-02-04 21:47:44 +0530

    the new feature - phone search

--- js/app.js
+++ js/app.js
@@ -15,10 +15,10 @@ $(document).ready(function () {
     document.querySelector(".loading")?.classList?.remove("d-flex");
   }
 
-  $("#btn-search").on("click", function (e) {
+  $("#btn-email-search").on("click", function (e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
-    email = $('input[type="text"]').val().toLowerCase();
+    email = $('input[type="email"]').val().toLowerCase();
 
     var x, y;
     regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -29,7 +29,7 @@ $(document).ready(function () {
     }
 
     if (x === true) {
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+      document.querySelector('input[type="email"]').parentNode.classList.remove("error");
       const proxyurl = "";
       const url = "https://ltv-data-api.herokuapp.com/api/v1/records.json?email=" + email;
       showLoader();
@@ -42,16 +42,16 @@ $(document).ready(function () {
         })
         .catch((e) => console.log(e));
     } else if (x !== true) {
-      document.querySelector('input[type="text"]').parentNode.classList.add("error");
+      document.querySelector('input[type="email"]').parentNode.classList.add("error");
     }
   });
 
-  $('input[type="text"]').keypress(function (event) {
-    email = $('input[type="text"]').val().toLowerCase();
+  $('input[type="email"]').keypress(function (event) {
+    email = $('input[type="email"]').val().toLowerCase();
     regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     if (email.match(regEx)) {
       x = true;
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+      document.querySelector('input[type="email"]').parentNode.classList.remove("error");
     } else {
       x = false;
     }
@@ -79,7 +79,77 @@ $(document).ready(function () {
           })
           .catch((e) => console.log(e));
       } else if (x !== true) {
-        document.querySelector('input[type="text"]').parentNode.classList.add("error");
+        document.querySelector('input[type="email"]').parentNode.classList.add("error");
+      }
+    }
+  });
+
+  // Mobile
+  $("#btn-phone-search").on("click", function (e) {
+    e.preventDefault();
+    localStorage.clear(); //Clears storage for next request
+    phone = $('input[type="tel"]').val().toLowerCase();
+
+    var x, y;
+    regEx = /^\d{10}$/;
+    if (phone.match(regEx)) {
+      x = true;
+    } else {
+      x = false;
+    }
+
+    if (x === true) {
+      document.querySelector('input[type="tel"]').parentNode.classList.remove("error");
+      const proxyurl = "";
+      const url = "https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=" + phone;
+      showLoader();
+      fetch(proxyurl + url)
+        .then((response) => response.text())
+        .then(function (contents) {
+          localStorage.setItem("userObject", contents);
+          hideLoader();
+          window.location.href = "result.html";
+        })
+        .catch((e) => console.log(e));
+    } else if (x !== true) {
+      document.querySelector('input[type="tel"]').parentNode.classList.add("error");
+    }
+  });
+
+  $('input[type="tel"]').keypress(function (event) {
+    phone = $('input[type="tel"]').val().toLowerCase();
+    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (phone.match(regEx)) {
+      x = true;
+      document.querySelector('input[type="tel"]').parentNode.classList.remove("error");
+    } else {
+      x = false;
+    }
+    keycode = event.keyCode ? event.keyCode : event.which;
+    if (keycode == "13") {
+      /**
+       * Makes a request to ltv API to search an specific phone address.
+       * If there's a response, it gets stored in the local storage and redirects to results page
+       */
+      event.preventDefault();
+      localStorage.clear(); //Clears storage for next request
+
+      var x, y;
+
+      if (x === true) {
+        const proxyurl = "";
+        const url = "https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=" + phone;
+        showLoader();
+        fetch(proxyurl + url)
+          .then((response) => response.text())
+          .then(function (contents) {
+            localStorage.setItem("userObject", contents);
+            hideLoader();
+            window.location.href = "result.html";
+          })
+          .catch((e) => console.log(e));
+      } else if (x !== true) {
+        document.querySelector('input[type="tel"]').parentNode.classList.add("error");
       }
     }
   });
